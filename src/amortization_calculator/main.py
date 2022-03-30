3c27c4ab6d54dd16166b3ea2a4ce6ff1f7cd24c8
Erik Thorsell <erik@thorsell.io>
2022-03-30 06:29:29 +0200

    Change how amortization is calculated

--- src/amortization_calculator/main.py
+++ src/amortization_calculator/main.py
@@ -11,7 +11,7 @@ def _parse_args():
     parser.add_argument(
         "--stop-amortizing",
         action="store_true",
-        help="Use if you want to stop amortizing when your debt quota is <4.5 and your loan is <50% of the house's value.",
+        help="Use if you want to stop amortizing when your debt quota is <4.5 and your debt is <50% of the house's value.",
     )
     parser.add_argument("--minimum-amortization", type=int, help="If specified, the minimum amortization per month.")
     parser.add_argument("--years", type=int, default=60, help="Number of years to calculate for.")
@@ -36,10 +36,11 @@ def main() -> None:
     down_payment = args.down_payment
     interest = args.interest
 
-    loan = house_price - down_payment
+    principal = house_price - down_payment
+    debt = principal
 
     print(f"Monthly income: {monthly_income} | Yearly income: {yearly_income}")
-    print(f"House price: {house_price} | Loan: {loan} | Down payment: {down_payment} | Interest: {interest}")
+    print(f"House price: {house_price} | Principal: {principal} | Down payment: {down_payment} | Interest: {interest}")
     print(f"Lagfart: {house_price*0.015:.0f}", end="")
     if args.pantbrev:
         print(
@@ -53,23 +54,23 @@ def main() -> None:
     month_counter = 0
     while month_counter / 12 < args.years:
 
-        interest_payment = (loan * (interest / 100)) / 12
+        interest_payment = (debt * (interest / 100)) / 12
         tot_interest_payment += interest_payment
 
         if month_counter % 12 == 0:
-            if loan / house_price > 0.7:
+            if debt / house_price > 0.7:
                 amortization_percentage = 2
-            elif loan / house_price > 0.5:
+            elif debt / house_price > 0.5:
                 amortization_percentage = 1
             else:
                 amortization_percentage = 0
 
             print(f"\nYear ({month_counter/12:.0f}). ", end="")
             print(
-                f"The loan / house price ratio is: {(loan/house_price)*100:.0f}%. ",
+                f"The debt / house price ratio is: {(debt/house_price)*100:.1f}%. ",
                 end="",
             )
-            debt_quota = loan / yearly_income
+            debt_quota = debt / yearly_income
             print(f"Debt quota is: {debt_quota:.2f}. ", end="")
 
             if debt_quota > 4.5:
@@ -78,11 +79,11 @@ def main() -> None:
 
             if amortization_percentage == 0 and args.stop_amortizing:
                 print(
-                    "Your loan is down to 50% of the house's value and your debt quota is <4.5. You have opted to stop amortizing!"
+                    "Your debt is down to 50% of the house's value and your debt quota is <4.5. You have opted to stop amortizing!"
                 )
                 break
 
-        loan_payment = (loan * (amortization_percentage / 100)) / 12
+        loan_payment = (principal * (amortization_percentage / 100)) / 12
         loan_payment = max(loan_payment, args.minimum_amortization) if args.minimum_amortization else loan_payment
 
         if loan_payment == 0:
@@ -93,12 +94,12 @@ def main() -> None:
             )
             break
 
-        loan -= loan_payment
+        debt -= loan_payment
         print(
-            f"Month: {month_counter:3} | Interest: {interest_payment:5.0f}, Amortization ({amortization_percentage}%): {loan_payment:5.0f} | Monthly payment: {interest_payment + loan_payment + args.running_cost:7.0f} | Remaining debt: {loan:7.0f}, Total interest: {tot_interest_payment:7.0f}"
+            f"Month: {month_counter:3} | Interest: {interest_payment:5.0f}, Amortization ({amortization_percentage}%): {loan_payment:5.0f} | Monthly payment: {interest_payment + loan_payment + args.running_cost:7.0f} | Remaining debt: {debt:7.0f}, Total interest: {tot_interest_payment:7.0f}"
         )
 
-        if loan < 0:
+        if debt < 0:
             print(f"You have payed of your debt!")
             break
 
